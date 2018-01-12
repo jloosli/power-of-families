429676b00103043775000d55a3de1c13bb22a666
Jared Loosli <jloosli@gmail.com>
2018-01-12 16:15:35 -0700

    Updated missing image icon

--- power-of-families/php-scripts/Avanti/WooCommerce.class.php
+++ power-of-families/php-scripts/Avanti/WooCommerce.class.php
@@ -20,12 +20,14 @@ class WooCommerce
         // Replace add to card button with learn more in the store page
         add_action('init', [$this, 'remove_loop_button']);
         add_action('woocommerce_after_shop_loop_item', [$this, 'replace_add_to_cart']);
+
+        add_filter('gettext', [$this, 'change_billing_details_to_your_details']);
     }
 
     function woocommerce_after_shop_loop_item_title_short_description()
     {
         global $product;
-        $excerpt=$product->get_short_description();
+        $excerpt = $product->get_short_description();
         if (!$excerpt) return;
         ?>
         <div itemprop="description">
@@ -44,5 +46,15 @@ class WooCommerce
         global $product;
         $link = $product->get_permalink();
         echo do_shortcode('<br><a class="button" href="' . esc_attr($link) . '">Learn More</a>');
+    }
+
+    function change_billing_details_to_your_details($translated_text, $text, $domain)
+    {
+        switch (strtolower('Billing Details')) {
+            case 'billing details':
+                $translated_text = __('Your details', 'woocommerce');
+                break;
+        }
+        return $translated_text;
     }
 }
