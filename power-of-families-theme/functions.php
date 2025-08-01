083437cd769ed74ef6c6052be5f2f9f7ce4f92cd
Jared Loosli <jloosli@gmail.com>
2025-08-01 13:19:42 -0600

    Removed errant theme

--- power-of-families-theme/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once('inc/Autoloader.class.php');
-$Autoloader = new \POF\Autoloader();
-$ThemeSetup = new \Avanti\ThemeSetup();
-
-new \Avanti\WooCommerce();
-new \Avanti\Readme();
-
--- power-of-families-theme/src/main.ts
+++
@@ -1,5 +0,0 @@
-import {Development} from "./development";
-import {ResponsiveMenu} from './responsive-menu';
-
-new Development();
-new ResponsiveMenu();
