5aeeb7502c416c403029c4f6d370e0ace17adf2f
Jared Loosli <jloosli@gmail.com>
2025-08-16 16:13:54 -0600

    Added in Programs and cleaned up unused parts

--- power-of-families/src/main.ts
+++ power-of-families/src/main.ts
@@ -1,7 +1,6 @@
 import { Development } from './development';
 import { ResponsiveMenu } from './responsive-menu';
 import '../assets/css/main.css';
-import './programs/public.scss';
 
 new Development();
 new ResponsiveMenu();
