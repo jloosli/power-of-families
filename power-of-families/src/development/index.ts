a1a360b35d7d8549e42df85aee0485680f8d598e
Jared Loosli <jloosli@gmail.com>
2025-09-05 15:56:51 -0600

    Final view changes

--- power-of-families/src/development/index.ts
+++ power-of-families/src/development/index.ts
@@ -1,32 +1,38 @@
 class Development {
-  constructor() {
-    document.addEventListener('DOMContentLoaded', ()=> setTimeout(()=> this.useServerImagesIfDevelopingLocally(), 500));
-  }
+	constructor() {
+		document.addEventListener('DOMContentLoaded', () =>
+			setTimeout(() => this.useServerImagesIfDevelopingLocally(), 500)
+		);
+	}
 
-  useServerImagesIfDevelopingLocally() {
-    const ProductionRegex = /\.com$/;
-    const BeginningOfStringRegex = /^\//;
-    if (!ProductionRegex.test(window.location.hostname)) {
-      const images = document.querySelectorAll('img');
-      images.forEach((img) => {
-        if (img.src !== img.currentSrc) {
-          img.src = this.createUrlToImage(img.src);
-          if (img.srcset) {
+	useServerImagesIfDevelopingLocally() {
+		const ProductionRegex = /\.com$/;
+		const BeginningOfStringRegex = /^\//;
+		if (!ProductionRegex.test(window.location.hostname)) {
+			const images = document.querySelectorAll('img');
+			images.forEach((img) => {
+				if (img.src !== img.currentSrc) {
+					img.src = this.createUrlToImage(img.src);
+					if (img.srcset) {
+						img.srcset = img.srcset
+							.split(',')
+							.map((str) => str.trim())
+							.map((src) =>
+								BeginningOfStringRegex.test(src)
+									? 'https://poweroffamilies.com' + src
+									: src
+							)
+							.join(', ');
+					}
+				}
+			});
+		}
+	}
 
-            img.srcset = img.srcset.split(',')
-              .map(str => str.trim())
-              .map(src => BeginningOfStringRegex.test(src) ? 'https://poweroffamilies.com' + src : src)
-              .join(", ");
-          }
-        }
-      });
-    }
-  }
-
-  createUrlToImage(location: string) {
-    const url = new URL(location);
-    return 'https://poweroffamilies.com' + url.pathname;
+	createUrlToImage(location: string) {
+		const url = new URL(location);
+		return 'https://poweroffamilies.com' + url.pathname;
+	}
 }
-}
 
-export {Development}
+export { Development };
---
+++ power-of-families/src/woocommerce/index.ts
@@ -0,0 +1,26 @@
+/**
+ * Adds link to the learn more button on product archive pages.
+ * IMPORTANT: In order for this to work, a button with class "learn-more-button"
+ * must be added to the Product Template on the landing page for the store
+ */
+
+const context = 'data-wp-context';
+
+function addLinkToLearnMoreButton() {
+    		document.addEventListener('DOMContentLoaded', () =>
+			setTimeout(() => addLinks())
+		);
+}
+function addLinks() {
+    document.querySelectorAll('.learn-more-button').forEach(theDiv => {
+        const productString = theDiv.closest(`[${context}]`)?.getAttribute(context);
+        if (!productString) return;
+        const productId = JSON.parse(productString)?.productId;
+        // console.log({ productId });
+        const theButton = theDiv.querySelector('a');
+        if (!theButton) return;
+        theButton.setAttribute('href', `/?page_id=${productId}`);
+    });
+}
+
+export { addLinkToLearnMoreButton };
